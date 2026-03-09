45c39a91be2f6fb8121a6674d69ff48318a450f7
goranbulatovicsrb-cyber <goranbulatovicsrb@gmail.com>
2026-03-09 12:09:47 +0100

    Add files via upload

---
+++ src/all.cpp
@@ -0,0 +1,7 @@
+// Unity build - kompajlira sve u jednom fajlu
+#include "orthodoxholidays.cpp"
+#include "eventmanager.cpp"
+#include "musicplayer.cpp"
+#include "calendarwidget.cpp"
+#include "mainwindow.cpp"
+#include "main.cpp"
